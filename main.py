05352e55c3a28b6ba5c8a6579ba24675220e6641
Abdelqader Okasha <abd20160772@std.psut.edu.jo>
2022-02-18 18:39:08 +0200

    Changed character selection key to Enter, and adjusted text in player selection screen

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import copy
 # A "thing" is basically anything that can be shown to the screen in the game
 # such as characters, backgrounds, or text
 class Thing:
-    image_path = "assets/not_found.jpg" # by default, a thing has the "not found" image displayed
+    image_path = "assets/not_found.jpg"  # by default, a thing has the "not found" image displayed
     image = pygame.image.load("assets/not_found.jpg")
     text = ""
     textSurface = None
-    isText = False # if this thing is a text, this value becomes true
+    isText = False  # if this thing is a text, this value becomes true
     fontSize = 30
     pos_x = 0
     pos_y = 0
@@ -30,7 +30,7 @@ class Thing:
 
     # method called to make the 'thing' draw itself on the window
     def draw_self(self, window):
-        if self.isText: # if this thing was a text
+        if self.isText:  # if this thing was a text
             # Try assigning text
             try:
                 myFont = pygame.font.SysFont('Comic Sans MS', self.fontSize)
@@ -38,10 +38,10 @@ class Thing:
             except:
                 myFont = pygame.font.SysFont('Comic Sans MS', 30)
                 self.textSurface = myFont.render("No Text", False, (0, 0, 0))
-            #draw the text
+            # draw the text
             window.blit(self.textSurface, (self.pos_x, self.pos_y))
 
-        else: # else if it wasnt a text, then its an image
+        else:  # else if it wasnt a text, then its an image
             # Try assigning image path
             try:
                 self.image = pygame.image.load(self.image_path)
@@ -51,6 +51,7 @@ class Thing:
                 scaled_image = pygame.transform.scale(self.image, (self.size_x, self.size_y))
 
             window.blit(scaled_image, (self.pos_x, self.pos_y))
+
 
 # ************************************* end of class 'thing' definition **************************************
 
@@ -71,17 +72,32 @@ redGreenLightSound = pygame.mixer.Sound("assets/red_green_sound_short.wav")
 # pygame font is used to display text
 pygame.font.init()
 
-#Frames per second
+# Frames per second
 FPS = 100
 clock = pygame.time.Clock()
 
 # state is used to see which level the game is in,
 # innerState is used to see which part of the level the game is in
-state = 0 # 0- Intro screen | 1- Character selection Screen | 2- Level 1 | 3- Level 2 | 4- Level 3 | 4- Winner screen
+state = 0  # 0- Intro screen | 1- Character selection Screen | 2- Level 1 | 3- Level 2 | 4- Level 3 | 4- Winner screen
 innerState = 0
 
 # a list of things to draw on screen, empty for now
 things = list()
+
+
+def generate_multi_line_text(s, pos_x, pos_y, font_size):
+    generated_text_list = []
+    current_y = pos_y
+    stream = ""
+    for char in s:
+        if char != '\n':
+            stream += char
+        else:
+            generated_text_list.append(Thing("", pos_x, current_y, 0, 0, True, stream, font_size))
+            stream = ""
+            current_y += font_size
+    return generated_text_list
+
 
 # things that can be added
 # we are creating them using the class 'thing', and its __init__ method (see line 21)
@@ -93,8 +109,11 @@ thing_character_nadeen = Thing("assets/char_nadeen.png", 0, 0, 100, 180, False)
 thing_character_hashem = Thing("assets/char_hashem.png", 0, 0, 100, 180, False)
 thing_character_triangle = Thing("assets/char_triangle.png", 0, 0, 100, 180, False)
 thing_character_circle = Thing("assets/char_circle.png", 0, 0, 100, 180, False)
-thing_selectChar_text = Thing("", 450, 50, 0, 0, True, "Select your characters", 30)
-thing_logo = Thing("assets/logo.png",400,50,300,300,False)
+thing_selectChar_text_list = generate_multi_line_text("Select your characters\n"
+                                                      "with A and D for player 1\n"
+                                                      "and Arrows for player 2\n"
+                                                      "Press Enter to continue", 450, 50, 30)
+thing_logo = Thing("assets/logo.png", 400, 50, 300, 300, False)
 
 # TODO add remaining things as needed
 
@@ -107,7 +126,6 @@ player2_selection = 1
 
 # countdown value to wait between events
 countdown = 180  # 180 frames
-
 
 # Main game loop starts here
 while True:
@@ -142,35 +160,37 @@ while True:
     elif state == 1:  # Character Selection
         # empty all things to draw
         things.clear()
+        print("At state 1")
 
         # add background and text
         things.append(thing_intro_background)
-        things.append(thing_selectChar_text)
+        for thing in thing_selectChar_text_list:
+            things.append(thing)
+        print(len(things))
 
-        available_characters = [thing_character_saif, thing_character_nadeen, thing_character_hashem, thing_character_triangle, thing_character_circle]
+        available_characters = [thing_character_saif, thing_character_nadeen, thing_character_hashem,
+                                thing_character_triangle, thing_character_circle]
         # by default, both characters are set to 0
 
         # player1 switches character by using A,D keys
         # player2 switches character by using left,right keys
 
         for event in events:
-            if event.type == pygame.KEYDOWN: # if a key is pressed
+            if event.type == pygame.KEYDOWN:  # if a key is pressed
 
-                if event.key == pygame.K_d: # if the pressed key is (d)
-                    player1_selection = player1_selection + 1 # increment player 1 selection by 1
-                    if player1_selection > (len(available_characters) - 1): # if player 1 selection reaches above the number of available characters
-                        player1_selection = 0 # cycle back to first character (number 0)
+                if event.key == pygame.K_d:  # if the pressed key is (d)
+                    player1_selection = player1_selection + 1  # increment player 1 selection by 1
+                    if player1_selection > (len(
+                            available_characters) - 1):  # if player 1 selection reaches above the number of available characters
+                        player1_selection = 0  # cycle back to first character (number 0)
 
-                if event.key == pygame.K_a: # and the pressed key is (a)
-                    player1_selection = player1_selection - 1 # decrement player 1 selection by 1
-                    if player1_selection < 0: # if player 1 selection reaches number 0
-                        player1_selection = len(available_characters) - 1 # cycle back to last character
+                if event.key == pygame.K_a:  # and the pressed key is (a)
+                    player1_selection = player1_selection - 1  # decrement player 1 selection by 1
+                    if player1_selection < 0:  # if player 1 selection reaches number 0
+                        player1_selection = len(available_characters) - 1  # cycle back to last character
 
-
-
-                # TODO (HASHEM) do the same for player 2 (Using keys right and left)
+                # TODO the same for player 2 (Using keys right and left)
                 # hint: same code as above, but change player1 to 2, and they keys to arrows
-
 
         # after selections were made, assign each player a copy of the character they selected
         player1_character = copy.deepcopy(available_characters[player1_selection])
@@ -185,27 +205,24 @@ while True:
         things.append(player2_character)
 
         # Check if space is pressed
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_RETURN]:
             innerState = 0  # reset inner state
             state = 2  # go to state 2 (level 1)
-
-        # TODO add key listener to confirm selection and move to next state (SAIF)
-        # hint: check how we did this in previous state (line 135)
 
     # **************************** STATE 2: LEVEL 1 (RED LIGHT GREEN LIGHT)  ****************************
 
     elif state == 2:  # Level 1  (Red light, Green Light)
-
+        print("at state 2")
         if innerState == 0:  # GET READY SCREEN
             things.clear()
 
-            # TODO the following: (FARHAN)
+            # TODO the following:
             # empty list of things
             # Add the level 1 background to list of things
             # Add the 2 selected characters to list of things, positioned at the left of screen
             # hint: see the beginning of state 1 to check how we add things
 
-            # TODO the following: (NADEEN)
+            # TODO the following:
             # Add a text "GET READY" to list of things , in the middle of screen
             # Add the "Doll" character to list of things, positioned at the right of screen
             # hint: see the beginning of state 1 to check how we add things
@@ -253,10 +270,8 @@ while True:
         pass
 
     # **************************** STATE 5: WINNER SCREEN  ****************************
-    elif state == 5:  #  Winner screen
+    elif state == 5:  # Winner screen
         pass
-
-
 
     # black out the screen
     window.fill((0, 0, 0))
